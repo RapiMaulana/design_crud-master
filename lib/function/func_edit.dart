6feb3cdfaf3fa4ba414f7b2d43472799d78dfd8a
Rapi Maulana <kangrapi2@gmail.com>
2023-01-08 06:42:25 +0700

    commit

---
+++ lib/function/func_edit.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'dart:developer';
+import 'func_isLoading.dart';
+
+editData(context, id, jdl, description, pathImg) async {
+  //loading lalu pushnamed ke fungsi read
+  isLoading(context);
+  String? imgName;
+  var dataImg;
+  if (pathImg != null) {
+    imgName = pathImg.path.split('/').last;
+    dataImg = await MultipartFile.fromFile(pathImg.path,
+        filename: imgName.toString());
+  } else {
+    dataImg = null;
+  }
+
+  FormData sendData = FormData.fromMap({
+    "idnews": id.toString(),
+    "judul": jdl.toString(),
+    "deskripsi": description.toString(),
+    "url_image": dataImg,
+  });
+  final response = await Dio()
+      .post("https://sipenma.000webhostapp.com/editnews.php", data: sendData);
+  return Navigator.of(context)
+      .pushNamedAndRemoveUntil('/read', (Route<dynamic> route) => false);
+}
